0383cbff440950dd9175ecb92abd17e3348c684a
faramozmangal <faramoz360@gmail.com>
2022-08-25 16:13:38 -0400

    bye World added

---
+++ src/first/byeWorld.java
@@ -0,0 +1,9 @@
+package first;
+
+public class byeWorld {
+
+    public static void main(String[] args) {
+        System.out.println("Maybe 5 weeks of Java left");
+        System.out.println("1.5 weeks of git");
+    }
+}
